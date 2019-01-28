537d3c20d27e35fca8f63c5a1a54a6dfc191bcc9
rpscruz <rscruz7@up.edu,ph>
2019-01-28 14:35:07 +0800

    CS 192 Project: Q

---
+++ Q/app/src/main/java/com/example/rc/q/AddPlayerFragment.java
@@ -0,0 +1,22 @@
+package com.example.rc.q;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AddPlayerFragment extends Fragment {
+
+    public AddPlayerFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_player, container, false);
+    }
+}
